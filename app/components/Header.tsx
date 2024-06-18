1c55b9e9cf540d77788f6c3122972435a077b4de
María Torrente <caritotorrente@gmail.com>
2024-06-18 16:52:39 -0400

    clean-up, missing use user when is no oauth profile, lets do a hook for both?

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,32 +1,55 @@
-import { Link } from "@remix-run/react";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import VerseBytesLogo from "assets/verse-bytes-light.png";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export default function Header() {
+  const navItems = [
+    { name: "Logout", path: "/logout" },
+    { name: "Blog", path: "/blog" },
+  ];
+
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
   return (
-    <nav className="flex items-center justify-between bg-gray-800 text-white p-4">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl">Verse Bytes</span>
-      </div>
-      <div className="block lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <Link
-            to="/login"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4"
+    <nav className="bg-gray-800 text-white px-8 py-10 w-full">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <h1 className="text-5xl font-semibold">Verse Bytes</h1>
+        </div>
+        <div className="flex items-center justify-center lg:hidden">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="focus:outline-none"
           >
-            Login
-          </Link>
-          <Link
-            to="/logout"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4"
-          >
-            Logout
-          </Link>
-          <Link
-            to="/blog"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300"
-          >
-            Blog
+            <Bars3Icon className="h-6 w-6" />
+          </button>
+        </div>
+        <div>
+          <Link to="/login">
+            <img
+              src={VerseBytesLogo}
+              alt="Verse Bytes Logo"
+              className="h-10 w-10 transition-transform transform hover:scale-110"
+            />
           </Link>
         </div>
+      </div>
+      <div
+        className={`lg:flex justify-center items-center text-xl ${
+          menuOpen ? "block mt-4" : "hidden"
+        } lg:block lg:mt-0`}
+      >
+        <ul className="flex flex-col lg:flex-row lg:items-center lg:space-x-4">
+          {navItems.map((item) => (
+            <li
+              key={item.name}
+              className="text-white hover:text-gray-300 transition-colors duration-300 mb-4 lg:mb-0"
+            >
+              <Link to={item.path}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
